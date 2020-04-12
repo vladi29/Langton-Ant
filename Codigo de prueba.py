788fe1c9f13e28d8caf66c7e5cda2d1f5339062c
vladi29 <42014792+vladi29@users.noreply.github.com>
2020-04-12 19:07:54 -0400

    Ya se logro crear un tablero cuadriculado
    Se logro crear un tablero cuadriculado, pa esto se dise;o el iterador de manera que solo es necesario agregar las dimesiones del cuadro y de cada casilla

--- Codigo de prueba.py
+++ Codigo de prueba.py
@@ -1,6 +1,43 @@
-while True:
-    try:
-        Dimension = int(input('Por favor ingresa la dimension de tu tablero: '))
-        break
-    except(TypeError, ValueError):
-        print('Hey! Eso no es un numero.\nIntentalo otra vez.')
+import pygame, sys
+from pygame.locals import *
+from random import randint
+
+pygame.init()                           # A partir de esta linea funcionaran todas las instrucciones de pygame
+
+DimVentana = 500
+Ventana = pygame.display.set_mode((DimVentana,DimVentana))
+pygame.display.set_caption('Programa de prueba')
+
+azul = pygame.Color(50,50,200)
+negro = pygame.Color(0,0,0)
+blanco = pygame.Color(255,255,255)
+Alto = 10
+Ancho = 10
+margen = 4
+
+
+def Iterador(anch,marg,DimVent):
+    i = 0
+    n = 0
+    a = anch + marg
+    while n < DimVent:
+        yield n
+        i += 1
+        n = i*a
+
+#Dim = Iterador(Ancho,margen,DimVentana)    Usando esto arroja error ya que esto es una direccion de memoria en donde estan los valores del iterador
+#print(*Dim)
+
+#Dim = [0 ,25, 50, 75, 100, 125, 150, 175, 200, 225, 250]
+#print(Dim)
+
+while True:                             # Loop infinito para mantener abierta la ventana
+    for Evento in pygame.event.get():       # Se hara un recorrido a traves de los diferentes tipos de eventos que tiene la libreria
+        if Evento.type == QUIT:
+            pygame.quit()
+            sys.exit
+    for columna in Iterador(Ancho,margen,DimVentana):
+        for fila in Iterador(Ancho,margen,DimVentana):
+            pygame.draw.rect(Ventana,negro,(columna,fila,margen,Alto))
+            pygame.draw.rect(Ventana,azul,(columna + margen,fila + margen, Ancho,Alto))
+    pygame.display.update()
--- LangtonAnt.py
+++ LangtonAnt.py
@@ -4,17 +4,29 @@
 
 import pygame, sys
 from pygame.locals import *
-
-color1 = (20, 0, 50)  #Morado // Tener cuidado porque es un elemento de tipo tupla y no tipo color
-color2 = pygame.Color(255,255,255)  #Naranja // Este si es tipo color
+from random import randint
 
 pygame.init()                           # A partir de esta linea funcionaran todas las instrucciones de pygame
 
-A = 700
-Al = 350
-Ventana = pygame.display.set_mode((A,Al))
+color1 = pygame.Color(128,64,0)     #Madera
+color2 = pygame.Color(34,113,179)   #Azul claro
+color3 = pygame.Color(155,155,155)  #Gris
+AnVen = 700
+AlVen = 400
+AnTab = 300
+AlTab = 300
+(Xi,Yi) = (AnVen/2 - AnTab/2, AlVen/2 - AlTab/2)
+Ventana = pygame.display.set_mode((AnVen,AlVen))
 pygame.display.set_caption('Hormiga de Langton')
+Ventana.fill(color1)
 
+pygame.draw.rect(Ventana,color3,(Xi,Yi, AnTab,AlTab))  #tablero de 200x200 pixeles
+pygame.draw.line(Ventana, color2, (Xi-2,Yi-2), (Xi+AnTab+2,Yi-2), 4)
+pygame.draw.line(Ventana, color2, (Xi+AnTab+2,Yi-2), (Xi+AnTab+2,Yi+AlTab+2), 4)
+pygame.draw.line(Ventana, color2, (Xi+AnTab+2,Yi+AlTab+2), (Xi-2,Yi+AlTab+2) , 4)
+pygame.draw.line(Ventana, color2, (Xi-2,Yi+AlTab+2), (Xi-2,Yi-2) , 4)
+
+"""
 #-----Solicitud de Dimensiones del tablero-----
 while True:
     try:
@@ -23,17 +35,10 @@ while True:
     except(TypeError, ValueError):
         print('Hey! Eso no es un numero.\nIntentalo otra vez.')
 #-----Final de la solicitud-----
-
-#print(color2.r)      #Como extraer la saturacion roja de un color
-#print(color2.g)      #Como extraer la saturacion verde de un color
-#print(color2.b)      #Como extraer la saturacion azul de un color
-
-#Dibujar diferentes poligonos:
-
+"""
 
 
 while True:                             # Loop infinito para mantener abierta la ventana
-
     for Evento in pygame.event.get():       # Se hara un recorrido a traves de los diferentes tipos de eventos que tiene la libreria
         if Evento.type == QUIT:
             pygame.quit()
